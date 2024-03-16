8c6631447e5ea48ece286e6655a4a101f831c40a
unknown <raginithakrele@gmail.com>
2024-03-16 16:44:43 +0530

    logic added for one-to-one mapping between customer and address

--- bank-service/src/main/java/com/bank/service/CustomerService.java
+++ bank-service/src/main/java/com/bank/service/CustomerService.java
@@ -2,10 +2,14 @@ package com.bank.service;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bank.dao.CustomerInterface;
+import com.bank.entity.Address;
 import com.bank.entity.Customer;
 import com.bank.repository.CustomerRepository;
 
@@ -15,10 +19,19 @@ public class CustomerService implements CustomerInterface {
 	@Autowired
 	private CustomerRepository repository;
 
+	@Autowired
+	AddressService service;
+
+	Address addressId;
+
 	@Override
 	public Customer saveCustomer(Customer customer) {
+
+		addressId=service.saveAddress(customer.getAddress());
 		return repository.save(customer);
 	}
+
+	
 
 	public List<Customer> getAllCustomer() {
 		return repository.findAll();
